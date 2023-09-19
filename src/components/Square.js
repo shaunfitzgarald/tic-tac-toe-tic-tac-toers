bbba5414139f5fc07b6273c2ea45d2a80bd2ef33
malsherer <mgracesherer@gmail.com>
2023-09-19 13:02:42 -0700

    worked on interactive features

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,17 +1,22 @@
-import React, {useState} from 'react'
+import React from "react"
 
-const Square = () => {
-  const [value, setValue] = useState(null)
-  const handleClick = () => {
-    setValue("❌")
-
-  }
+const Square = ({value}) => {
   return (
-    <>
-        <button className="square"
-        onClick={onSquareClick}
-        >{value}</button>
-    </>
+<div className="board-row">
+<App value={squares[0]} onSquareclick={() => handleClick(0)}/>
+<App value={squares[1]} onSquareclick={() => handleClick(1)}/>
+<App value={squares[2]} onSquareclick={() => handleClick(2)}/>
+</div>
+<div className="board-row">
+<Square value={squares[3]} onSquareclick={() => handleClick(3)}/>
+<Square value={squares[4]} onSquareclick={() => handleClick(4)}/>
+<Square value={squares[5]} onSquareclick={() => handleClick(5)}/>
+</div>
+<div className="board-row">
+<Square value={squares[6]} onSquareclick={() => handleClick(6)}/>
+<Square value={squares[7]} onSquareclick={() => handleClick(7)}/>
+<Square value={squares[8]} onSquareclick={() => handleClick(8)}/>
+</div>
   )
 }
 
