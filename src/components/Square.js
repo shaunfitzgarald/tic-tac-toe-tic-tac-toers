577f292229b80247c4f659306d5ad6f5d40c361c
LouisBrizuela <louisbrizz@gmail.com>
2023-09-19 15:45:16 -0700

    revert back to original

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,23 +1,24 @@
-import React from "react"
+import React from 'react'
 
-const Square = ({value}) => {
-  return (
-<div className="board-row">
-<App value={squares[0]} onSquareclick={() => handleClick(0)}/>
-<App value={squares[1]} onSquareclick={() => handleClick(1)}/>
-<App value={squares[2]} onSquareclick={() => handleClick(2)}/>
-</div>
-<div className="board-row">
-<Square value={squares[3]} onSquareclick={() => handleClick(3)}/>
-<Square value={squares[4]} onSquareclick={() => handleClick(4)}/>
-<Square value={squares[5]} onSquareclick={() => handleClick(5)}/>
-</div>
-<div className="board-row">
-<Square value={squares[6]} onSquareclick={() => handleClick(6)}/>
-<Square value={squares[7]} onSquareclick={() => handleClick(7)}/>
-<Square value={squares[8]} onSquareclick={() => handleClick(8)}/>
-</div>
-  )
+const Square = ({squares, handleClick}) => {
+  console.log("string", squares)
+return (
+<>
+<div className="grid">
+{squares.map((value, index) => {
+  console.log("value", index)
+ return (
+  <div 
+  className="square" 
+  key={index}
+  onClick={() => handleClick(index)}
+ >
+  {value} 
+ </div>
+ )
+ })}
+  </div>
+</>)
 }
-
 export default Square
+
